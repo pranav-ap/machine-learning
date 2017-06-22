c1a838d3888d8251beda4e8e3d61657a4781f4e0
AP Pranav <pranavap96@gmail.com>
2017-06-22 17:24:00 +0530

    natural language processing for review classification is done

--- Apriori_Python/apriori.py
+++ Apriori_Python/apriori.py
@@ -7,13 +7,22 @@ import pandas as pd
 
 # Data Preprocessing
 dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+
 transactions = []
 for i in range(0, 7501):
     transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
 
+#transactions =  dataset.values.tolist()
+                                     
 # Training Apriori on the dataset
 from apyori import apriori
-rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2) 
 
 # Visualising the results
 results = list(rules)
+
+#results_list = []
+#for i in range(0, len(results)):
+#    results_list.append('RULE:\t' + str(results[i][0]) + '\nSUPPORT:\t' + str(results[i][1]) + '\n' + str(results[i][2]))
+    
+myResults = [list(x) for x in results] 
